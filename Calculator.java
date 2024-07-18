66b2abb3f436aed09d092652c1cbaa1662e7daf5
GoranshSh9 <118844948+GoranshSh9@users.noreply.github.com>
2024-07-19 00:37:39 +0530

    updates

--- Calculator.java
+++ Calculator.java
@@ -5,9 +5,11 @@ public class Calculator {
         Scanner in = new Scanner(System.in);
         int ans = 0;
         while (true) {
+            System.out.println("Enter the Operation: ");
             char op = in.next().trim().charAt(0);
 
             if (op == '+' || op == '-' || op == '*' || op == '/' || op == '%') {
+                System.out.println("Enter the numbers: ");
                 int num1 = in.nextInt();
                 int num2 = in.nextInt();
 
@@ -24,12 +26,17 @@ public class Calculator {
                     if (num2 != 0) {
                         ans = num1 / num2;
                     }
+                }
                 if (op == '%') {
                         ans = num1 % num2;
                     }    
-                }
+            } else if (op == 'x' || op == 'X') {
+                break;
+            } else {
+                System.out.println("Invalid Operation!!");
             }
+        System.out.println("Answer: " + ans);
         }
-        System.out.println(ans);
+    in.close();
     }
 }
