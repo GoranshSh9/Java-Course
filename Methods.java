1377dcd6d99ad7b0b7ff6dd90b6806273d0ef7e4
GoranshSh9 <118844948+GoranshSh9@users.noreply.github.com>
2024-07-27 23:58:37 +0530

    Update Methods.java

--- Methods.java
+++ Methods.java
@@ -6,6 +6,7 @@ public class Methods {
         Scanner in = new Scanner(System.in);
         int a = in.nextInt();
         System.out.println(a);
+        int a = 1000;
         in.close();
     }
 }
