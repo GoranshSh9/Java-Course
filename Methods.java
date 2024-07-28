021715b0a27ee68eb7183b427518a0a55a75e58d
GoranshSh9 <118844948+GoranshSh9@users.noreply.github.com>
2024-07-28 14:26:21 +0530

    Prime or not

--- Methods.java
+++ Methods.java
@@ -4,9 +4,23 @@ import java.util.Scanner;
 public class Methods {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int a = in.nextInt();
-        System.out.println(a);
-        int b = 10 ;
+        int n = in.nextInt();
+        boolean ans = isPrime(n);
+        System.out.println(ans);
         in.close();
+    }
+
+    static boolean isPrime(int n) {
+        if (n <= 1){
+            return false;
+        }
+        int c = 2;
+        while (c * c <= n) {
+            if (n % c == 0){
+                return false;
+            }
+            c++;
+        }
+        return c*c>n;
     }
 }
