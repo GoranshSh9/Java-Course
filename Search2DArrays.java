db859775b1df872d08f255b2edd09384e4a54a13
GoranshSh9 <118844948+GoranshSh9@users.noreply.github.com>
2024-07-30 21:15:49 +0530

    Search in 2D arrays

---
+++ Search2DArrays.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class Search2DArrays {
+    public static void main(String[] args) {
+        int[][] arr = {
+            {1, 2, 3},
+            {12, 23, 34, 45},
+            {56, 67, 78},
+            {22, 33}
+        };
+        int target = 67;
+        int[] ans = search(arr, target);
+        System.out.println(Arrays.toString(ans));
+    }
+    static int[] search(int[][] arr, int target){
+        for(int row = 0; row < arr.length; row++){
+            for(int col = 0; col<arr[row].length; col++){
+                if (arr[row][col] == target) {
+                    return new int[]{row, col};
+                }
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
